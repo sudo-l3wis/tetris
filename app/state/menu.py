1faf4360a849f73a58013a77451ab99764ee99b0
sudo-l3wis <self@l3wis.dev>
2021-05-09 11:36:57 +0100

    Resized assets

--- app/state/menu.py
+++ app/state/menu.py
@@ -14,10 +14,26 @@ class MenuState(State):
         pass
 
     def render(self, surface):
-        draw(surface, 'sprites.board', (0, 0))
-        draw(surface, 'sprites.pieces.I', (0, 0))
-        draw(surface, 'sprites.box')
-        draw(surface, 'sprites.extended_box')
+        w = config('display.width')
+        h = config('display.height')
+
+        bw = config('sprites.box.width')
+        bh = config('sprites.box.height')
+
+        offsetX = w - bw - 15
+
+        draw(surface, 'sprites.box', (offsetX, 9))
+        draw(surface, 'sprites.extended_box', (offsetX, bh + 20))
+
+        draw(surface, 'sprites.board', (10, 10))
+
+
         #draw(surface, 'sprites.button')
         #draw(surface, 'sprites.icon_play')
-        #draw(surface, 'sprites.pieces.I', (20, 20))
+        #draw(surface, 'sprites.pieces.I', (0, 0))
+        #draw(surface, 'sprites.pieces.J', (0, 0))
+        #draw(surface, 'sprites.pieces.L', (0, 0))
+        #draw(surface, 'sprites.pieces.O', (0, 0))
+        #draw(surface, 'sprites.pieces.S', (0, 0))
+        draw(surface, 'sprites.pieces.T', (0, 0))
+        #draw(surface, 'sprites.pieces.Z', (0, 0))
