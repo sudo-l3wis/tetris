c5e04025b79d2eb61caad15ee73a520c459cd65f
sudo-l3wis <self@l3wis.dev>
2021-05-20 00:18:50 +0100

    Fixed right collision

--- app/events/controls/right.py
+++ app/events/controls/right.py
@@ -2,13 +2,21 @@ class MoveRightEvent:
 
     def handle(self, grid, _next):
         cols = config('display.grid.cols')
-
         item = grid.current()
-        gx = item.get_grid_x()
-        gw = item.get_grid_width()
 
         if item is not None:
-            if gx + gw < cols:
+            x, y, w, h = item.pos()
+
+            if x + w >= cols:
+                return next(_next)
+
+            move = True
+            for j in range(h):
+                for i in range(w):
+                    if item.is_fill(i, j) and grid.cells[y+j][x+i+1] is not None:
+                        move = False
+
+            if move:
                 item.inc_grid_x(1)
 
         return next(_next)
