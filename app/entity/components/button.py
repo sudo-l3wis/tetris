9fca58b933a04b6586b1857b867f6fea38aca6de
sudo-l3wis <self@l3wis.dev>
2021-05-25 22:04:41 +0100

    Adjusted button icon render

--- app/entity/components/button.py
+++ app/entity/components/button.py
@@ -4,21 +4,36 @@ from app.types import ButtonState
 
 class Button(Component):
 
-    def __init__(self):
+    def __init__(self, icon):
         super().__init__()
+        self.icon = icon
         self.handler = None
         self.state = None
         self.is_pressed = False
-
-    def set(self, surface):
-        conf = config(surface)
-        bounds = tuple(conf.values())
-        self.set_surface(asset('sprites').subsurface(bounds))
-        self.set_width(conf['width'])
-        self.set_height(conf['height'])
+        self.icon = surface('icon_{}'.format(icon))
+        self.set_width(config('sprites.button.width'))
+        self.set_height(config('sprites.button.height'))
 
     def handle(self, handler):
         self.handler = handler
+
+    def render(self, surface):
+        if self.state is ButtonState.DEFAULT:
+            draw(surface, 'sprites.button', self.get_pos())
+        elif self.state is ButtonState.HOVER:
+            draw(surface, 'sprites.button_hover', self.get_pos())
+        elif self.state is ButtonState.PRESSED:
+            draw(surface, 'sprites.button_pressed', self.get_pos())
+
+        w, h = self.icon.get_size()
+
+        offset_x = (self.get_width() - w) / 2
+        offset_y = (self.get_height() - h) / 2
+
+        pos = (offset_x + self.get_x(), offset_y + self.get_y())
+
+        surface.blit(self.icon, pos)
+
 
     def update(self, delta):
         super().update(delta)
@@ -30,19 +45,15 @@ class Button(Component):
 
     def on_mount(self):
         self.state = ButtonState.DEFAULT
-        self.set('sprites.button')
 
     def on_hover(self):
         self.state = ButtonState.HOVER
-        self.set('sprites.button_hover')
 
     def on_leave(self):
         self.state = ButtonState.DEFAULT
-        self.set('sprites.button')
 
     def on_press(self):
         self.state = ButtonState.PRESSED
-        self.set('sprites.button_pressed')
         self.is_pressed = True
 
     def on_move(self, pos):
